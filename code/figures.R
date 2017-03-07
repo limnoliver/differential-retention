fc9fddf661b22edbe1128c43ba623b65bc59e86d
limnoliver <skoliver@wisc.edu>
2017-03-07 12:28:56 -0600

    outlined figures I want to make for budget lakes

---
+++ code/figures.R
@@ -0,0 +1,23 @@
+# This script creates figures for differential retention project
+
+# Figure 1: N and P retention according to Vollenweider & Harrison
+# Figure 2: Differential retention according to V & H
+# Figure 3: Differential retention with realistic depth and residence time
+# plotted on figure - deciles from mass balance lakes?
+# Figure 4: Mass balance lakes - depth vs residence time
+# Figure 5: Mass balance lakes - histograms of N and P retention
+# Figure 5.123: histograms of broken up by N retention > P retention,
+# histogram of size, input concentrations, etc.
+# Figure 6: N vs P retention
+# Figure 7: Differential retention with deciles for depth and retention time + 
+# real N and P retention
+# Figure 8: Differential retention as % change in TN:TP vs rank or res time
+# - predicted from models
+# Figure 9: Differential retention as % change in TN:TP vs rank or res time
+# - observed
+# Figure 10: Same as Figure 9, but with color = rank or res time and x = start concentration
+# figure 11: Same as figure 9, but broken up into 3 - each panel represents low, medium
+# high res time/rank, and x = start ratio or start P concentration (to test Finlay hypothesis?)
+# Figure 12: calculate Vf and sedimentation coefficient and summarize by lake type. 
+# show how sed coef or Vf changes with nutrient input concentration
+# 
