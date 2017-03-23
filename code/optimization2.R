8dd4a168928d2eafabeaac75bec2eba498fe1221
limnoliver <skoliver@wisc.edu>
2017-03-23 17:02:49 -0500

    added nitrogen model and optimization

--- code/optimization2.R
+++ code/optimization2.R
@@ -6,6 +6,16 @@ upperBound = c(10,0)
 a.start = 1.12
 b.start = -0.52
 
+# use all values where res time and retention are available for Rp
+dat.p <- dat.all[!is.na(dat.all$Rp)&!is.na(dat.all$res_time)&!is.na(dat.all$tp_in_conc)&!is.na(dat.all$tp_out_conc), ]
+dat.p.real <- dat.p[dat.p$Rp>-1, ]
+dat.p.pos <- dat.p[dat.p$Rp>0, ]
+
+dat.n <- dat.all[!is.na(dat.all$Rn)&!is.na(dat.all$res_time)&!is.na(dat.all$mean_depth), ]
+dat.n.real <-dat.n[dat.n$Rn>=-1,]
+dat.n.pos <-dat.n[dat.n$Rn>=0,]
+
+
 # a.start = start value for par a (educated guess; 1.12 from Brett & Benjamin)
 # b.start = best guess for par b (-0.53 from Brett & Benjamin)
 # dat.in = data frame with all variables needed (tp_in_conc, tp_out_conc, Rp, res_time)
@@ -16,6 +26,7 @@ P.ret.mod.fit <- function(dat.in, type) {
 a <- a.start
 b <- b.start
 pars = c(a,b)
+parStart = pars
 
 d <- dat.in
 
@@ -64,6 +75,40 @@ Fit.Rp <- P.ret.mod.fit(dat.in = dat.p, type = "retention")
 Fit.Rp.real <- P.ret.mod.fit(dat.in = dat.p.real, type = "retention")
 Fit.Rp.pos <- P.ret.mod.fit(dat.in = dat.p.pos, type = "retention")
 
+Vf.start = 8.91
+lowerBound = 0
+upperBound = 1000
+N.ret.mod.fit <- function(dat.in){
+  Vf <- Vf.start
+  pars = Vf
+  parStart = pars
+  d <- dat.in
+  zz <- d$mean_depth
+  tt <- d$res_time
+  yy <- d$Rn
+    model <- function(Vf){
+      z = zz
+      t = tt
+      y = 1 - exp(-Vf*(tt/zz))
+    }
+    
+    out_diff <- function(pars){
+      modeled = model(pars)
+      resTN = modeled - yy
+      return(resTN)
+    }
+    Fit <- modFit(f = out_diff, p=parStart,method = 'Newton', 
+                  lower= lowerBound,
+                  upper= upperBound)
+    return(Fit)
+}
+Fit.N <- N.ret.mod.fit(dat.n)
+Fit.N.real <- N.ret.mod.fit(dat.n.real)
+Fit.N.pos <- N.ret.mod.fit(dat.n.pos)
+har.all <- har[!is.na(har$Rn_calculated)&!is.na(har$res_time)&!is.na(har$mean_depth), ]
+har.all$Rn <- har.all$Rn_calculated
+Fit.N.harrison <- N.ret.mod.fit(har.all)
+
 curve(1-(1/(1+(Fit.Rp.real$par[1]*(x^(1+Fit.Rp.real$par[2]))))), 0.001, 1000, log = "x",
       ylab = "Retention", xlab = "Residence Time (y)", 
       col = "red", ylim = c(-1, 1.1), lwd = 4,xaxt = "n", cex.lab = 2, cex.axis = 1.3)
@@ -76,15 +121,30 @@ curve(1-(1/(1+(Fit.real$par[1]*(x^(1+Fit.real$par[2]))))), 0.001, 1000, log = "x
       ylab = "Retention", xlab = "Residence Time (y)", 
       col = "orange", ylim = c(-1, 1.1), lwd = 4,xaxt = "n", 
       cex.lab = 2, cex.axis = 1.3, add = TRUE)
-
-yy = dat.p.pos$Rp
-tt = dat.p.pos$res_time
-Fit.Rp.pos <- modFit(f = TP_out_diff, p=parStart,method = 'Newton', 
-                      lower= lowerBound,
-                      upper= upperBound)
 curve(1-(1/(1+(Fit.Rp.pos$par[1]*(x^(1+Fit.Rp.pos$par[2]))))), 0.001, 1000, log = "x",
       ylab = "Retention", xlab = "Residence Time (y)", 
       col = "blue", ylim = c(-1, 1.1), lwd = 4,xaxt = "n", 
       cex.lab = 2, cex.axis = 1.3, add = TRUE)
 
+# N fit 
+curve(1-(exp(-Fit.N.harrison$par*(x/5.9))), 0.001, 1000, log = "x",
+ylab = "Retention", xlab = "Residence Time (y)", 
+col = col.n, ylim = c(-1, 1.1), lwd = 4,xaxt = "n", cex.lab = 2, cex.axis = 1.3)
 
+curve(1-(1/(1+(Fit.Brett$par[1]*(x^(1+Fit.Brett$par[2]))))), 0.001, 1000, log = "x",
+      ylab = "Retention", xlab = "Residence Time (y)", 
+      col = col.p, ylim = c(-1, 1.1), lwd = 4,xaxt = "n", 
+      cex.lab = 2, cex.axis = 1.3, add = TRUE)
+curve(1-(1/(1+(Fit.real$par[1]*(x^(1+Fit.real$par[2]))))), 0.001, 1000, log = "x",
+      col = col.p, ylim = c(-1, 1.1), lwd = 4,xaxt = "n", lty = 2,
+      cex.lab = 2, cex.axis = 1.3, add = TRUE)
+curve(1-(exp(-Fit.N.real$par*(x/5.9))), 0.001, 1000, log = "x",
+      col = col.n, ylim = c(-1, 1.1), lwd = 4,xaxt = "n", 
+      cex.lab = 2, cex.axis = 1.3, lty = 2, add = TRUE)
+curve(1-(exp(-Fit.N.real$par*(x/3))), 0.001, 1000, log = "x",
+      col = col.n, ylim = c(-1, 1.1), lwd = 4,xaxt = "n", 
+      cex.lab = 2, cex.axis = 1.3, lty = 2, add = TRUE)
+curve(1-(exp(-Fit.N.real$par*(x/11))), 0.001, 1000, log = "x",
+      col = col.n, ylim = c(-1, 1.1), lwd = 4,xaxt = "n", 
+      cex.lab = 2, cex.axis = 1.3, lty = 2, add = TRUE)
+points(dat.n$Rn~dat.n$res_time, log = "x", cex = 1, pch = 21, bg =rgb(111,111,111,111,max=250))
